694cb5fb186df430ebe73fdadae5dbe868ee077a
Samar Yadav <xtricity@Samars-MacBook-Pro.local>
2024-07-17 15:24:24 +0530

    struct

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 import streamlit as st
 import pandas as pd
-st.title("New App")
-df = pd.DataFrame([[1,2],[4,5]], columns = ["ID", "ID2"], index=[0,1])
-st.write("Hello World", df)
-st.header("Header with a :rainbow[divider]", divider= 'rainbow')
-st.subheader(":red[Subheader] :sunglasses: _italics_", divider = "blue")
-st.markdown(""" # h1 tag
-## h2 tag
-### h3 tag 
-:moon: <br> 
-:sunflower:""", True)
-st.latex(r''' a + ar + a r^2 \cdots + a r^{n-1} ''')
+import numpy as np
+
+st.markdown("""
+    <style>
+            
+    .main .block-container {
+        padding-top: 2rem;
+        padding-bottom: 1rem;
+    }        
+    .stButton button {
+        margin: auto;
+        display: block;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+
+st.markdown("<h1 style='text-align: center;'>Forecasting Dashboard</h1>", unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
+
+st.markdown("<h6 style='text-align: center;'>Select a model to forecast</h6>", unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
+
+col1,col2,col3,col4 = st.columns(4)
+
+with col1:
+    st.button("SARIMA", key="sarima_button")
+with col2:
+    st.button("Prophet", key="prophet_button")
+with col3:
+    st.button("LGBM", key="lgbm_button")
+with col4:
+    st.button("XGBoost", key="xgboost_button")
+
+    
+
+
